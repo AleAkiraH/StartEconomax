35b71822612b837dff88294371dff20a654f0cdb
Automações <automacao.desenvolvimento@gmail.com>
2023-05-08 22:25:12 -0300

    dsa

--- start.js
+++ start.js
@@ -3,34 +3,31 @@ const axios = require('axios');
 function fazerRequisicao(url) {
     setInterval(() => {
         realizarRequisicao(url);
-    }, 4 * 60 * 1000 + 30 * 1000); // 4 minutos e 30 segundos em milissegundos
+    }, 1000); // 4 minutos e 30 segundos em milissegundos
 }
 
 async function realizarRequisicao(url) {
-    // Se estiver no ambiente de produção, faz a requisição
-    if (process.env.NODE_ENV === 'production') {
-        try {
-            const data = {
-                usuario: 'aleakirah',
-                senha: '14191712'
-            };
+    try {
+        const data = {
+            usuario: 'aleakirah',
+            senha: '14191712'
+        };
 
-            const response = await axios.post(url, data);
+        const response = await axios.post(url, data);
 
-            if (response.status === 200) {
-                console.log(`Requisição bem-sucedida! (${url})`);
-                const horario = new Date().toISOString();
-                console.log(`Requisição feita às ${horario}`);
-            } else {
-                console.log(`Erro na requisição. (${url})`);
-                const horario = new Date().toISOString();
-                console.log(`Requisição feita às ${horario}`);
-            }
-        } catch (error) {
+        if (response.status === 200) {
+            console.log(`Requisição bem-sucedida! (${url})`);
+            const horario = new Date().toISOString();
+            console.log(`Requisição feita às ${horario}`);
+        } else {
             console.log(`Erro na requisição. (${url})`);
             const horario = new Date().toISOString();
             console.log(`Requisição feita às ${horario}`);
         }
+    } catch (error) {
+        console.log(`Erro na requisição. (${url})`);
+        const horario = new Date().toISOString();
+        console.log(`Requisição feita às ${horario}`);
     }
 }
 
