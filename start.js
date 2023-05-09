a003a7030a58eb0749505fcac412b9ccbd5b6da5
Automações <automacao.desenvolvimento@gmail.com>
2023-05-08 22:33:42 -0300

    dsassss

--- start.js
+++ start.js
@@ -1,9 +1,14 @@
 const axios = require('axios');
+const express = require('express');
+
+// Defina a variável quantidade
+let quantidade = 12;
 
 function fazerRequisicao(url) {
+    realizarRequisicao(url); // Executa imediatamente
     setInterval(() => {
         realizarRequisicao(url);
-    }, 1000); // 4 minutos e 30 segundos em milissegundos
+    }, 4 * 60 * 1000 + 30 * 1000); // 4 minutos e 30 segundos em milissegundos
 }
 
 async function realizarRequisicao(url) {
@@ -30,6 +35,19 @@ async function realizarRequisicao(url) {
         console.log(`Requisição feita às ${horario}`);
     }
 }
+
+// Crie uma instância do Express
+const app = express();
+
+// Defina a rota para GET
+app.get('/', (req, res) => {
+    res.send(`Quantidade: ${quantidade}`);
+});
+
+// Inicie o servidor Express
+app.listen(3000, () => {
+    console.log('Servidor iniciado na porta 3000');
+});
 
 fazerRequisicao('https://economax.onrender.com/login');
 fazerRequisicao('https://economax-dev.onrender.com/login');
