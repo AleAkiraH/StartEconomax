fff9d6825b6d567af9bd2b625a599b2f4edd6131
Automações <automacao.desenvolvimento@gmail.com>
2023-05-08 21:51:42 -0300

    production

--- start.js
+++ start.js
@@ -1,38 +1,41 @@
 const axios = require('axios');
 
-function fazerRequisicao(url) {    
-    realizarRequisicao(url);
+function fazerRequisicao(url) {
     setInterval(() => {
-        console.log(`Executei ${url}`);
         realizarRequisicao(url);
     }, 4 * 60 * 1000 + 30 * 1000); // 4 minutos e 30 segundos em milissegundos
 }
 
 async function realizarRequisicao(url) {
-    try {
-        const data = {
-            usuario: 'aleakirah',
-            senha: '14191712'
-        };
+    // Se estiver no ambiente de produção, faz a requisição
+    if (process.env.NODE_ENV === 'production') {
+        try {
+            const data = {
+                usuario: 'aleakirah',
+                senha: '14191712'
+            };
 
-        const response = await axios.post(url, data);
+            const response = await axios.post(url, data);
 
-        if (response.status === 200) {
-            console.log(`Requisição bem-sucedida! (${url})`);
-            const horario = new Date().toISOString();
-            console.log(`Requisição feita às ${horario}`);
-        } else {
+            if (response.status === 200) {
+                console.log(`Requisição bem-sucedida! (${url})`);
+                const horario = new Date().toISOString();
+                console.log(`Requisição feita às ${horario}`);
+            } else {
+                console.log(`Erro na requisição. (${url})`);
+                const horario = new Date().toISOString();
+                console.log(`Requisição feita às ${horario}`);
+            }
+        } catch (error) {
             console.log(`Erro na requisição. (${url})`);
             const horario = new Date().toISOString();
             console.log(`Requisição feita às ${horario}`);
         }
-    } catch (error) {
-        console.log(`Erro na requisição. (${url})`);
-        const horario = new Date().toISOString();
-        console.log(`Requisição feita às ${horario}`);
     }
 }
 
-// Inicia as requisições em paralelo
-fazerRequisicao('https://economax.onrender.com/login');
-fazerRequisicao('https://economax-dev.onrender.com/login');
+// Verifica se está no ambiente de produção antes de iniciar as requisições
+if (process.env.NODE_ENV === 'production') {
+    fazerRequisicao('https://economax.onrender.com/login');
+    fazerRequisicao('https://economax-dev.onrender.com/login');
+}
