28426bff1a606a06213764e40b4086a5ff9f46c4
Automações <automacao.desenvolvimento@gmail.com>
2023-05-08 22:56:39 -0300

    zzzzzzzz

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+
+let quantidade = 12;
+
+function fazerRequisicao(url) {
+  realizarRequisicao(url);
+  setInterval(() => {
+      realizarRequisicao(url);
+  }, 4 * 60 * 1000 + 30 * 1000);
+}
+
+async function realizarRequisicao(url) {
+  try {
+      const data = {
+          usuario: 'aleakirah',
+          senha: '14191712'
+      };
+
+      const response = await axios.post(url, data);
+
+      if (response.status === 200) {
+          console.log(`Requisição bem-sucedida! (${url})`);
+          const horario = new Date().toISOString();
+          console.log(`Requisição feita às ${horario}`);
+      } else {
+          console.log(`Erro na requisição. (${url})`);
+          const horario = new Date().toISOString();
+          console.log(`Requisição feita às ${horario}`);
+      }
+  } catch (error) {
+      console.log(`Erro na requisição. (${url})`);
+      const horario = new Date().toISOString();
+      console.log(`Requisição feita às ${horario}`);
+  }
+}
+
+app.get('/', (req, res) => {
+  res.send(`Quantidade: ${quantidade}`);
 });
+
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
 // Export the Express API
 module.exports = app;
+
+fazerRequisicao('https://economax.onrender.com/login');
+fazerRequisicao('https://economax-dev.onrender.com/login');
